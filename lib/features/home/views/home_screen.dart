d8903d792b80163f6cfa5c9cb1c5428cbd1660ce
favourTy <favouradetayo03@gmail.com>
2025-01-22 02:27:42 +0100

    added loading state

--- lib/features/home/views/home_screen.dart
+++ lib/features/home/views/home_screen.dart
@@ -19,6 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final cache = CacheService();
 
   final apiRepo = ApiRepository();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -66,42 +67,57 @@ class _HomeScreenState extends State<HomeScreen> {
                 const SizedBox(
                   height: 30,
                 ),
-                FutureBuilder(
-                  builder: (_, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                          child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation(AppColors.appColor),
-                      ));
-                    } else if (snapshot.hasError) {
-                      return const Center(
-                          child: Text("Could not fetch categories"));
-                    } else if (snapshot.data?.error != null) {
-                      return Center(
-                        child: Text(snapshot.data?.error ??
-                            "Could not fetch categories"),
-                      );
-                    }
-                    return ListView.separated(
-                        physics: NeverScrollableScrollPhysics(),
-                        itemBuilder: (_, i) {
-                          final each = snapshot.data?.cats?[i];
-                          return CustomListTile(
-                              ontap: () {
-                                AppRouter.push(
-                                    AppRouteStrings.productListScreen,
-                                    arg: each);
+                isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation(AppColors.appColor),
+                        ),
+                      )
+                    : FutureBuilder(
+                        builder: (_, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            return const Center(
+                                child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation(AppColors.appColor),
+                            ));
+                          } else if (snapshot.hasError) {
+                            return const Center(
+                                child: Text("Could not fetch categories"));
+                          } else if (snapshot.data?.error != null) {
+                            return Center(
+                              child: Text(snapshot.data?.error ??
+                                  "Could not fetch categories"),
+                            );
+                          }
+                          setState(() {
+                            isLoading = true;
+                          });
+                          return ListView.separated(
+                              physics: NeverScrollableScrollPhysics(),
+                              itemBuilder: (_, i) {
+                                final each = snapshot.data?.cats?[i];
+                                return CustomListTile(
+                                    ontap: () {
+                                      AppRouter.push(
+                                          AppRouteStrings.productListScreen,
+                                          arg: each);
+                                    },
+                                    image: each?.image ?? "",
+                                    text: each?.name ?? "");
                               },
-                              image: each?.image ?? "",
-                              text: each?.name ?? "");
+                              separatorBuilder: (_, __) => const Divider(
+                                    height: 0,
+                                  ),
+                              itemCount: snapshot.data?.cats?.length ?? 0);
                         },
-                        separatorBuilder: (_, __) => const Divider(
-                              height: 0,
-                            ),
-                        itemCount: snapshot.data?.cats?.length ?? 0);
-                  },
-                  future: apiRepo.fetchCategories(),
-                ),
+                        future: apiRepo.fetchCategories(),
+                      ),
                 SizedBox(
                   height: 30,
                 ),
@@ -116,44 +132,61 @@ class _HomeScreenState extends State<HomeScreen> {
                 const SizedBox(
                   height: 30,
                 ),
-                FutureBuilder(
-                  builder: (_, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                          child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation(AppColors.appColor),
-                      ));
-                    } else if (snapshot.hasError) {
-                      return const Center(
-                          child: Text("Could not fetch categories"));
-                    } else if (snapshot.data?.error != null) {
-                      return Center(
-                        child: Text(snapshot.data?.error ??
-                            "Could not fetch categories"),
-                      );
-                    }
-                    return ListView.separated(
-                        physics: NeverScrollableScrollPhysics(),
-                        itemBuilder: (_, i) {
-                          final each = snapshot.data?.products?[i];
-                          return CustomListTile(
-                            ontap: () {
-                              AppRouter.push(AppRouteStrings.productListScreen,
-                                  arg: each);
-                            },
-                            trailingText: each?.price?.convertToNaira() ?? "",
-                            image: each?.images?.firstOrNull ?? "",
-                            text: each?.title ?? "",
-                            descriptionText: each?.description ?? "",
-                          );
+                isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation(AppColors.appColor),
+                        ),
+                      )
+                    : FutureBuilder(
+                        builder: (_, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            return const Center(
+                                child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation(AppColors.appColor),
+                            ));
+                          } else if (snapshot.hasError) {
+                            return const Center(
+                                child: Text("Could not fetch categories"));
+                          } else if (snapshot.data?.error != null) {
+                            return Center(
+                              child: Text(snapshot.data?.error ??
+                                  "Could not fetch categories"),
+                            );
+                          }
+                          setState(() {
+                            isLoading = true;
+                          });
+                          return ListView.separated(
+                              physics: NeverScrollableScrollPhysics(),
+                              itemBuilder: (_, i) {
+                                final each = snapshot.data?.products?[i];
+                                return CustomListTile(
+                                  ontap: () {
+                                    AppRouter.push(
+                                        AppRouteStrings.productDetailsScreen,
+                                        arg: each);
+                                  },
+                                  trailingText:
+                                      each?.price?.convertToNaira() ?? "",
+                                  image: each?.images?.firstOrNull ?? "",
+                                  text: each?.title ?? "",
+                                  descriptionText: each?.description ?? "",
+                                );
+                              },
+                              separatorBuilder: (_, __) => const Divider(
+                                    height: 0,
+                                  ),
+                              itemCount: snapshot.data?.products?.length ?? 0);
                         },
-                        separatorBuilder: (_, __) => const Divider(
-                              height: 0,
-                            ),
-                        itemCount: snapshot.data?.products?.length ?? 0);
-                  },
-                  future: apiRepo.fetchProducts(),
-                ),
+                        future: apiRepo.fetchProducts(),
+                      ),
               ],
             ),
           ),
